92545b3f5d5f7fee189a68f3f34f963f32ebf2b9
YanB <yanisbou324@gmail.com>
2026-02-05 10:07:46 +0100

    Kor + TempoSub
    Alors je sais pas si le TempoSub fonctionne a 100% mais c'est un début

---
+++ MobilePuzzleGame_Project/Assets/Scripts/Utils/CorThrowing.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CorThrowing : MonoBehaviour
+{
+    [SerializeField] private int beatToThrow = 1;
+    [SerializeField] private TempoSubscriber tempoSubscriber;
+    
+    UnityEvent onBallThrown; 
+    public float throwForce = 10f;
+    public float throwAngle = 45f;
+    public bool ballIn = false;
+    private int targetLayer;
+    private GameObject currentBall;
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        targetLayer = LayerMask.NameToLayer("Note");
+        onBallThrown = new UnityEvent();
+        
+        if (tempoSubscriber != null)
+        {
+            tempoSubscriber.onBeat.AddListener(OnBeatReceived);
+        }
+    }
+
+    public void OnBeatReceived(int beatIndex)
+{
+    if (!ballIn) return;
+    if (currentBall == null) return;
+    if (beatIndex != beatToThrow) return;
+
+    ThrowBall(currentBall);
+}
+
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.gameObject.layer == targetLayer)
+    {
+        other.GetComponent<SpriteRenderer>().enabled = false;
+        StartCoroutine(HandleBallEnter(other));
+    }
+}
+
+IEnumerator HandleBallEnter(Collider2D other)
+{
+    ballIn = true;
+    currentBall = other.gameObject;
+
+    LockBallPosition(currentBall);
+
+    var sr = other.GetComponent<SpriteRenderer>();
+    sr.enabled = false;
+    yield return new WaitForSeconds(0.5f);
+}
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == targetLayer)
+        {
+            ballIn = false;
+        }
+    }
+
+    public void ThrowBall(GameObject ball)
+    {
+        ball.GetComponent<SpriteRenderer>().enabled = true;
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        float angleInRadians = throwAngle * Mathf.Deg2Rad;
+        Vector2 throwDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians)).normalized;
+        rb.linearVelocity = throwDirection * throwForce;
+        ballIn = false;
+        onBallThrown?.Invoke();
+    }
+
+    void LockBallPosition(GameObject ball)
+    {
+        ball.transform.position = transform.position;
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+}
---
+++ MobilePuzzleGame_Project/Assets/Scripts/Utils/TempoSub.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class TempoSubscriber : MonoBehaviour
+{
+    public UnityEvent<int> onBeat = new UnityEvent<int>();
+
+    private Coroutine _waitRoutine;
+
+    private void OnEnable()
+    {
+        _waitRoutine = StartCoroutine(WaitForTempoManager());
+    }
+
+    private IEnumerator WaitForTempoManager()
+    {
+        while (TempoManager.Instance == null)
+            yield return null;
+
+        TempoManager.Instance.OnBeat += HandleBeat;
+        Debug.Log("ça marche");
+    }
+
+    private void OnDisable()
+    {
+        if (_waitRoutine != null)
+            StopCoroutine(_waitRoutine);
+
+        if (TempoManager.Instance != null)
+            TempoManager.Instance.OnBeat -= HandleBeat;
+    }
+
+    private void HandleBeat(int beatIndex)
+    {
+        onBeat.Invoke(beatIndex);
+    }
+}
