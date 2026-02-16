c1fccef358afc48456d05be1c9c789461ba0635d
Maxence <150053499+Scrapsss@users.noreply.github.com>
2026-02-16 17:26:21 +0100

    Accordeon

--- MobilePuzzleGame_Project/Assets/Scripts/Obstacles/Acordeon/AccordeonMiddle.cs
+++ MobilePuzzleGame_Project/Assets/Scripts/Obstacles/Acordeon/AccordeonMiddle.cs
@@ -8,10 +8,10 @@ public class AccordeonMiddle : MonoBehaviour
     [BoxGroup("GP"), SerializeField] private Transform _topPart;
     [BoxGroup("GP"), SerializeField] private Transform _bottomPart;
 
-    private void FixedUpdate()
+    private void Update()
     {
         transform.position = Vector2.Lerp(_topPart.position, _bottomPart.position, .5f);
-        transform.localScale = new Vector2(1, Vector2.Distance(_topPart.position, _bottomPart.position));
+        transform.localScale = new Vector2(transform.localScale.x, Vector2.Distance(_topPart.position, _bottomPart.position) / 3f);
     }
 
 }
